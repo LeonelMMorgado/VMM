b52a83ee27bcfb6000c676f41a2ecf5be5707e0c
Daniela <leonelmmorgado@gmail.com>
2025-08-11 17:59:57 -0300

    separa matvec

---
+++ include/projection.h
@@ -0,0 +1,7 @@
+#ifndef _PROJ_H
+#define _PROJ_H
+
+Mat4 mat4_orthogonal(float left, float right, float bottom, float top, float zNear, float zFar);
+Mat4 mat4_perspective(float fov, float aspect, float zNear, float zFar);
+
+#endif
